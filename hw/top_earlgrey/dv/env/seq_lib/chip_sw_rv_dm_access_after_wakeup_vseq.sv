9d3416a390aa4ef307e7ee7acd41a10f43fa07c6
Hugo McNally <hugo.mcnally@gmail.com>
2022-11-14 12:20:56 +0000

    h: moved into write and read into seperate tasks.

--- hw/top_earlgrey/dv/env/seq_lib/chip_sw_rv_dm_access_after_wakeup_vseq.sv
+++ hw/top_earlgrey/dv/env/seq_lib/chip_sw_rv_dm_access_after_wakeup_vseq.sv
@@ -16,12 +16,47 @@ class chip_sw_rv_dm_access_after_wakeup_vseq extends chip_sw_base_vseq;
     cfg.chip_vif.pwrb_in_if.drive(1'b1);
   endtask : pre_start
 
+  task write_and_readback_check(uvm_reg_data_t data, string error_suffix);
+    uvm_status_e   status;
+
+    // Write a value to the DMI program buffer.
+    cfg.jtag_dmi_ral.progbuf[0].write(
+      status,
+      data,
+      .path(UVM_FRONTDOOR),
+      .parent(this),
+      .fname(`__FILE__),
+      .lineno(`__LINE__)
+    );
+    `DV_CHECK_EQ(status, UVM_IS_OK,
+      {"Could not write to the RV_DM DMI program buffer 0 register ", error_suffix})
+
+    readback_check(data, error_suffix);
+
+  endtask : write_and_readback_check
+
+  task readback_check(uvm_reg_data_t data, string error_suffix);
+    uvm_status_e   status;
+    // Read the program buffer and check it still has the value that was
+    // written to it.
+    cfg.jtag_dmi_ral.progbuf[0].read(
+      status,
+      data,
+      .path(UVM_FRONTDOOR),
+      .parent(this),
+      .fname(`__FILE__),
+      .lineno(`__LINE__)
+    );
+    `DV_CHECK_EQ(status, UVM_IS_OK,
+      {"Could not read from the RV_DM DMI program buffer 0 register ", error_suffix})
+    `DV_CHECK_EQ(data, 'hDEAF,
+      {"RV_DM DMI program does not contain the expected value. ", error_suffix})
+  endtask : readback_check
+
   virtual task body();
     uint           timeout_long       = 10_000_000;
     uint           timeout_short      = 1_000_000;
     bit [7:0]      software_barrier[] = '{0};
-    uvm_status_e   status;
-    uvm_reg_data_t data;
 
     super.body();
 
@@ -34,32 +69,7 @@ class chip_sw_rv_dm_access_after_wakeup_vseq extends chip_sw_base_vseq;
     csr_wr(.ptr(cfg.jtag_dmi_ral.dmcontrol.dmactive), .value(1), .blocking(1), .predict(1));
     cfg.clk_rst_vif.wait_clks(5);
 
-    // Write a value to the DMI program buffer.
-    cfg.jtag_dmi_ral.progbuf[0].write(
-      status,
-      32'hDEAF,
-      .path(UVM_FRONTDOOR),
-      .parent(this),
-      .fname(`__FILE__),
-      .lineno(`__LINE__)
-    );
-    `DV_CHECK_EQ(status, UVM_IS_OK,
-      "Could not write to the RV_DM DMI program buffer 0 register")
-
-    // Read the program buffer and check it still has the value that was
-    // written to it.
-    cfg.jtag_dmi_ral.progbuf[0].read(
-      status,
-      data,
-      .path(UVM_FRONTDOOR),
-      .parent(this),
-      .fname(`__FILE__),
-      .lineno(`__LINE__)
-    );
-    `DV_CHECK_EQ(status, UVM_IS_OK,
-      "Could not read from the RV_DM DMI program buffer 0 register")
-    `DV_CHECK_EQ(data, 'hDEAF,
-      "RV_DM DMI program does not contain the expected value.")
+    write_and_readback_check(32'hDEAF, "first");
 
     // Allow the software to continue execution.
     software_barrier[0] = 1;
@@ -81,20 +91,7 @@ class chip_sw_rv_dm_access_after_wakeup_vseq extends chip_sw_base_vseq;
     `uvm_info(`gfn, "Releasing power button.", UVM_LOW)
     cfg.chip_vif.pwrb_in_if.drive(1'b1); // releasing power button
 
-    // Read the program buffer and check it still has the value that was
-    // written to it before the sleep.
-    cfg.jtag_dmi_ral.progbuf[0].read(
-      status,
-      data,
-      .path(UVM_FRONTDOOR),
-      .parent(this),
-      .fname(`__FILE__),
-      .lineno(`__LINE__)
-    );
-    `DV_CHECK_EQ(status, UVM_IS_OK,
-      "Could not read from the RV_DM DMI program buffer 0 register after sleep.")
-    `DV_CHECK_EQ(data, 'hDEAF,
-      "RV_DM DMI program does not contain the expected value after sleep.")
+    readback_check(32'hDEAF, "after sleep");
 
     // Allow the software to continue execution.
     software_barrier[0] = 2;
